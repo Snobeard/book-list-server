d89a8e412c79eb150b8f989767a4531d0ac39f48
Snobeard <fatmattmatt@gmail.com>
2017-11-07 09:38:13 -0800

    added /test route

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ const conString = 'postgres://postgres:1234@localhost:5432/postgres';
 const client = new pg.Client(conString);
 client.connect();
 
+app.get('/test', (req, res) => res.send('Hello World!'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
