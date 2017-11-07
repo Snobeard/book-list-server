aba47e55560c31eb85673c8a73565afa9faf97ce
Snobeard <fatmattmatt@gmail.com>
2017-11-07 11:44:37 -0800

    added database loader

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const pg = require('pg');
 const express = require('express');
-// const bodyParser = require('body-parser');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 
 const app = express();
@@ -15,4 +16,41 @@ client.connect();
 
 app.get('/test', (req, res) => res.send('Hello World!'));
 
+loadDB();
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+function loadBooks() {
+  client.query(`SELECT count(*) FROM books`)
+    .then(result => {
+      if (!parseInt(result.rows[0].count)){
+        fs.readFile('data/books.json', function(err, file) {
+          JSON.parse(file.toString()).forEach(book => {
+            client.query(`
+              INSERT INTO
+              books(title, author, isbn, image_url, description)
+                VALUES($1, $2, $3, $4, $5);`,
+              [book.title, book.author, book.isbn, book.image_url, book.description]
+            )
+          })
+        })
+      }
+    })
+}
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      book_id SERIAL PRIMARY KEY,
+      author VARCHAR(50),
+      title VARCHAR(50),
+      isbn VARCHAR(50),
+      image_url TEXT,
+      description TEXT
+    );`
+  )
+    .then(data => loadBooks(data))
+    .catch(err => console.log(err));
+}
